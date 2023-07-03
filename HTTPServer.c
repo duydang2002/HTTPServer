68ea24eac4c8f0660ff8f98b2ffc68911df9d71d
duydang2002 <76654292+duydang2002@users.noreply.github.com>
2023-07-03 22:30:20 +0700

    Update HTTPServer.c

--- HTTPServer.c
+++ HTTPServer.c
@@ -12,7 +12,7 @@
 #include <pthread.h>
 
 void *client_thread(void *);
-int port = 9009;
+int port = 9000;
 int main() 
 {
     int listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -80,7 +80,7 @@ void *client_thread(void *param)
         if (strcmp(uri,"/")==0){
             uri[0]=0;
         }
-    char *path= malloc(strlen(uri));
+    char *path= calloc(strlen(uri),1);
     path[0]='.';
     strcat(path, uri);
     puts(path);
@@ -97,10 +97,11 @@ void *client_thread(void *param)
     }
     struct dirent *entry;
     
-    char begin[256] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n";
+    char begin[256] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1> List </h1><p>";
     begin[strlen(begin)]=0;
+    char *end = "</p></body></html>";
+    send(client, begin, strlen(begin), 0);
 
-    char *msg = NULL;
     int size =0;
  
     while ((entry = readdir(dir)) != NULL)
@@ -115,13 +116,9 @@ void *client_thread(void *param)
             sprintf(fileName,"%s>%s</a></i><br>", href,entry->d_name);
             fileName[strlen(fileName)]=0;
 
+            send(client,fileName,strlen(fileName),0);
+            size+=strlen(fileName);
 
-            msg = realloc(msg,strlen(fileName)+size +1);
-            puts(msg);
-            size+= (int) strlen(fileName);
-            
-            strcat(msg,fileName);
-            msg[strlen(msg)]=0;
             free(fileName);
             free(href);
             fileName=0;
@@ -140,13 +137,9 @@ void *client_thread(void *param)
             char *dirName=calloc(512,1);
             sprintf(dirName,"%s>%s</a></b><br>", href,entry->d_name);
             dirName[strlen(dirName)]=0;
+            send(client,dirName,strlen(dirName),0);      
+            size+=strlen(dirName);       
 
-            msg = realloc(msg, size+ strlen(dirName)+1);
-            puts(msg);
-            size += (int) strlen(dirName);
-
-            strcat(msg, dirName);
-            msg[strlen(msg)]=0;
             free(dirName);
             free(href);
             dirName=0;
@@ -156,30 +149,78 @@ void *client_thread(void *param)
     }
     char contentLength[256];
     
-    char *end = "</p></body></html>";
+    
 
-    unsigned int length = strlen(msg)+strlen(end)+strlen(begin);
-    char len[64];
-    sprintf(len,"%u",length);
-    len[strlen(len)]=0;
-
-    sprintf(contentLength, "Content-Length: %s\r\n\r\n<html><body><h1> List </h1><p>", len);
-    char *Header = malloc((length+strlen(len))*sizeof(char)); 
-    strcat(Header,begin);
-    strcat(Header,contentLength);
-    strcat(Header,msg);
-    strcat(Header,end);
-    puts(Header);
-    send(client,Header,strlen(Header),0);
+    send(client,end,strlen(end),0);
     free(dir);
     dir = 0;
     free(path);
     }
-    else {
-        char *path= malloc(strlen(uri)+1);
+    else if (strcmp(uri,"/favicon.ico")==0){close(client);}
 
+    else {
+
+   
+    char *path= calloc(strlen(uri),1);
+    path[0]='.';
+    strcat(path, uri);
+    puts(path);
+
+    char begin[256]="HTTP/1.1 200 OK\r\nContent-Type: text/html";
+    begin[strlen(begin)]=0;
+    send(client,begin,strlen(begin),0);
+    // \r\n\r\n<html><body><h1> %s </h1><p>",path
+
+    // char *end = "</p></body></html>";   
+
+    FILE *file= fopen(path,"rb");
+    if (file==NULL){
+        char *msg = malloc(strlen("HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<html><body><h1>404 Not Found</h1><p>")+1);
+        strcat(msg,"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<html><body><h1>404 Not Found</h1><p>");
+        send(client, msg, strlen(msg),0);
+        free(msg);
+        free(path);
+        return NULL;
+    }
+    if (strstr(path,".txt") != NULL||strstr(path,".c") != NULL||strstr(path,".cpp") != NULL){
+    fseek(file, 0, SEEK_END);
+    long len = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char contentLength[512];
+    sprintf(contentLength,"Content-Length: %ld\r\n\r\n%s \n",len,path);
+    send(client,contentLength,strlen(contentLength),0);
+    char buf[256];
+    while (!feof(file)){
+        /* code */
+        int ret = fread(buf,1,sizeof(buf),file);
+        if (ret <= 0)
+        break;
+        send(client, buf, ret, 0);
+    }
+    // send(client,end,strlen(end),0);
+    fclose(file);
+    }
+     if (strstr(path,".jpg") != NULL||strstr(path,".png")!=NULL){
+        fseek(file, 0, SEEK_END);
+        long len = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        char contentLength[512];
+        sprintf(contentLength,"Content-Length: %ld\r\n\r\n%s \n",len,path);
+        puts(contentLength);
+        send(client,contentLength,strlen(contentLength),0);
+        char buf[256];
+        while (!feof(file)){
+            /* code */
+            int ret = fread(buf,1,sizeof(buf),file);
+            if (ret <= 0)
+            break;
+            
+            send(client, buf, ret, 0);
+            puts(buf);
+    }
     }
     
+}
+
     close(client);
-    
 }
